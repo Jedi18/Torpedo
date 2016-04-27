08e994dfd719547c0b6325d9ec5dcad770f416d3
RookieIndieDev <vigneshninja911@gmail.com>
2016-04-27 16:43:31 +0530

    Added Movement Code, Created a the Sub as a Prefab.

---
+++ Assets/Scripts/SubControlScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SubControlScript : MonoBehaviour 
+{
+	public Rigidbody2D SubRigidbody;
+	float MoveSpeed = 5f;
+
+	void Start ()
+	{
+		SubRigidbody = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () 
+	{	
+		if (Input.GetKey ("w")) 
+		{
+			SubRigidbody.velocity = new Vector2 (SubRigidbody.velocity.x, MoveSpeed);
+		}
+
+		if (Input.GetKey ("s")) 
+		{
+			SubRigidbody.velocity = new Vector2 (SubRigidbody.velocity.x, -MoveSpeed);
+		}
+
+		if (Input.GetKey ("a")) 
+		{
+			SubRigidbody.velocity = new Vector2 (-MoveSpeed,SubRigidbody.velocity.y );
+		}
+
+		if (Input.GetKey ("d")) 
+		{
+			SubRigidbody.velocity = new Vector2 (MoveSpeed,SubRigidbody.velocity.y );
+		}
+	}
+}
