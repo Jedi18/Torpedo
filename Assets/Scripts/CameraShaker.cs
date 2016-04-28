98d1b8a65b6381d711875b097ed5ccedd8e5870b
Jedi18 <targetakhil@gmail.com>
2016-04-28 15:21:57 +0530

    Camera Shake1
    Added camera script

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShaker : MonoBehaviour {
+
+	private float shakeTimer = 0.0f;
+	public float shakeTimerA = 0.5f;
+	public float shakeAmount = 0.5f;
+	public bool isShaking = false;
+	public GameObject theCamera;
+	
+	void Start()
+	{
+		isShaking = false;
+	}
+	
+	public void Update()
+	{
+		if (isShaking == true)
+		{
+			shakeTimer = shakeTimerA;
+		}
+		if (shakeTimer > 0)
+		{
+			
+			Vector2 ShakePos = Random.insideUnitCircle * shakeAmount;
+			
+			transform.position = new Vector2(transform.position.x + ShakePos.x, transform.position.y + ShakePos.y);
+			
+			shakeTimer -= Time.deltaTime;
+			
+			//Destroy(gameObject);
+		}
+	}
+
+	public void ShakeCamera(){
+
+		Debug.Log ("Shake the camera!");
+		isShaking = true;
+
+	}
+}
--- Assets/Scripts/TorpedoCollider.cs
+++ Assets/Scripts/TorpedoCollider.cs
@@ -1,61 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TorpedoCollider : MonoBehaviour
-{
+public class TorpedoCollider : MonoBehaviour {
 
-    public float shakeTimer;
-    public float shakeAmount;
-    //float shakeDur = 0.1f;
-    //float shakePwr = 1;
-    public bool isShaking = false;
-	public GameObject theCamera;
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		
+		if (col.gameObject.tag == "Player")
+		{
+			GameObject.Find("Main Camera").SendMessage("ShakeCamera");
+		}
 
-    void Start()
-    {
-        isShaking = false;
-        //ShakeCamera(shakeTimer, shakeAmount);
-        //theCamera = GameObject.Find ("Main Camera");
-    }
-
-    public void OnTriggerEnter2D(Collider2D col)
-    {
-
-        if (col.gameObject.tag == "Player")
-        {
-            isShaking = true;
-        }
-        else
-        {
-            isShaking = false;
-        }
-    }
-
-    public void Update()
-    {
-        if (isShaking == true)
-        {
-            ShakeCamera(shakeTimer, shakeAmount);
-        }
-        if (shakeTimer > 0)
-        {
-
-            Vector2 ShakePos = Random.insideUnitCircle * shakeAmount;
-
-           theCamera.transform.position = new Vector2(theCamera.transform.position.x + ShakePos.x, theCamera.transform.position.y + ShakePos.y);
-
-            shakeTimer -= Time.deltaTime;
-
-			//Destroy(gameObject);
-        }
-    }
-
-    public void ShakeCamera(float shakeDur, float shakePwr)
-    {
-
-        shakeAmount = shakePwr;
-        shakeTimer = shakeDur;
-    }
-
-
+	}
 }
