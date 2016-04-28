1c6ee628b7db1de88579cafb638b63bf2f5d1114
Jedi18 <targetakhil@gmail.com>
2016-04-28 21:40:51 +0530

    Simple playerinfo script

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInfo : MonoBehaviour {
+
+	public int health;
+	public float score;
+
+	// Use this for initialization
+	void Start () {
+		health = 3;
+		score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score += Time.deltaTime;
+	}
+
+	void reduceHealth(){
+		health--;
+		Debug.Log ("Health - "+ health);
+		Debug.Log("Score - "+ score);
+	}
+}
