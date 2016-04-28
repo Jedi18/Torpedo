9ab1ab3831a63a2ae2f7620eb491d7439084d306
Jedi18 <targetakhil@gmail.com>
2016-04-28 20:11:40 +0530

    I hope it works...?

---
+++ Assets/Scripts/mousesubcontrol.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mousesubcontrol : MonoBehaviour {
+
+	private Vector3 mousePosition;
+	public float distance = 5.0f;
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition.z = distance;
+		transform.position= Camera.main.ScreenToWorldPoint (Input.mousePosition);
+	
+	}
+
+}
