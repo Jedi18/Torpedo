dad5498ae2f7c33d56d149d164b3fc30eaee80bb
Jedi18 <targetakhil@gmail.com>
2016-04-28 19:54:34 +0530

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit b85c4787cdcddfce81988e916452e02fbd1a61d9.

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShaker : MonoBehaviour {
+
+	private float shakeTimer = 0.0f;
+	public float shakeTimerA = 0.5f;
+	public float shakeAmount = 0.5f;
+	public bool isShaking = false;
+	public GameObject theCamera;
+	
+	void Start()
+	{
+		isShaking = false;
+	}
+	
+	public void Update()
+	{
+		Vector3 originalPos = transform.position;
+
+		if (isShaking == true)
+		{
+			shakeTimer = shakeTimerA;
+		}
+		if (shakeTimer > 0)
+		{
+
+			Vector3 ShakePos = Random.insideUnitCircle * shakeAmount;
+			
+			transform.position = new Vector3(transform.position.x + ShakePos.x, transform.position.y + ShakePos.y,transform.position.z);
+			
+			shakeTimer -= Time.deltaTime;
+
+			isShaking = false;
+
+		}
+
+		if(shakeTimer == 0){
+			transform.position = originalPos;
+		}
+	}
+
+	public void ShakeCamera(){
+
+		isShaking = true;
+
+	}
+}
