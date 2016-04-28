ea7e3e23328a2f02f37282bab1e6fae1c65bfce7
Jedi18 <targetakhil@gmail.com>
2016-04-28 11:44:43 +0530

    Added torpedoes
    Torpedoes  look at and follow the player.

---
+++ Assets/Scripts/TorpedoMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TorpedoMovement : MonoBehaviour {
+
+	Vector3 playerPos;
+	GameObject player;
+	float moveSpeed = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Submarine-temp");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		playerPos = player.transform.position;
+
+		// To face the player
+		Vector3 dir = playerPos - transform.position;
+		var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+		// move towards the player
+		transform.Translate (Vector3.right * moveSpeed, Space.Self);
+	
+	}
+}
