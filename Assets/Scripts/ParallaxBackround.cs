65be3a47972585a789a0f74f92a49f9c09cd9a9d
WintrrWorks <wintrrworks@gmail.com>
2016-04-28 17:02:27 +1200

    Added Backround Scroll

---
+++ Assets/Scripts/ParallaxBackround.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxBackround : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        //GetComponent<Renderer>().material.mainTextureOffset = offset;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 offset = new Vector2(Time.time * -speed, 0);
+            GetComponent<Renderer>().material.mainTextureOffset = offset;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 offset = new Vector2(Time.time * speed, 0);
+            GetComponent<Renderer>().material.mainTextureOffset = offset;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector2 offset = new Vector2(0, Time.time * -speed);
+            GetComponent<Renderer>().material.mainTextureOffset = offset;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 offset = new Vector2(0, Time.time * speed);
+            GetComponent<Renderer>().material.mainTextureOffset = offset;
+        }
+    }
+}
