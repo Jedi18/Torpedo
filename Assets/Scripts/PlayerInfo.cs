cefcfdcc955a795e20f33d887c78845982ec729e
Jedi18 <targetakhil@gmail.com>
2016-04-29 10:44:57 +0530

    Revert "Merge remote-tracking branch 'refs/remotes/origin/master' into BetaSubControl"
    This reverts commit bf1037eded8dc61cf1c7fa49905268e51c67237b, reversing
    changes made to 1e80766db04fa6942e37469e800c3aa56703ac19.

--- Assets/Scripts/PlayerInfo.cs
+++ Assets/Scripts/PlayerInfo.cs
@@ -8,45 +8,21 @@ public class PlayerInfo : MonoBehaviour {
 	public float score = 0;
 	public Text scoreText;
 	public Text healthText;
-	public bool isAlive;
 
 	// Use this for initialization
 	void Start () {
 		scoreText.text = "Score - " + score;
 		healthText.text = "Lives - " + health;
-		isAlive = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (isAlive) {
-			score += Time.deltaTime;
-			scoreText.text = "Score - " + Mathf.Round (score);
-		}
+		score += Time.deltaTime;
+		scoreText.text = "Score - " + Mathf.Round(score);
 	}
 
 	void reduceHealth(){
 		health--;
 		healthText.text = "Lives - " + health;
-
-		if(health <= 0){
-			RIP();
-		}
-	}
-
-	void RIP(){
-		Debug.Log ("Game Over");
-		isAlive = false;
-		Debug.Log ("Score - " + Mathf.Round (score));
-		GetComponent<SubControlScript> ().enabled = false;
-		GameObject.Find("TorpedoSpawner").GetComponent<TorpedoSpawner>().enabled = false;
-		GameObject[] enemyList = GameObject.FindGameObjectsWithTag ("Enemy");
-
-		for (int i=0; i<enemyList.Length; i++) {
-
-			Destroy(enemyList[i]);
-
-		}
-
 	}
 }
