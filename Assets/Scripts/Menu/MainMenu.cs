e82a010a7250d114356057dda741e9d81afaea2e
Ben Clist <ben@benclist.com>
2016-04-30 16:18:36 +1200

    Basic Main Menu
    Basic menu with functionality to start the game and exit it.

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
