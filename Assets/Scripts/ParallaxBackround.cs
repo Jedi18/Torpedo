134479885d049977a8718de2a69d0addcf5c025a
Jedi18 <targetakhil@gmail.com>
2016-04-30 09:55:11 +0530

    Background issue

--- Assets/Scripts/ParallaxBackround.cs
+++ Assets/Scripts/ParallaxBackround.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class ParallaxBackround : MonoBehaviour {
 
     public float speed = 0.5f;
+	public GameObject player;
 
 	void Start ()
     {
-	
+		player = GameObject.FindWithTag ("Player");
 	}
 	
 	void Update ()
@@ -19,19 +20,23 @@ public class ParallaxBackround : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.A))
         {
-			offsetX = Time.time * -speed;
+			//offsetX = Time.time * -speed;
+			offsetX = player.transform.position.x;
         }
         if (Input.GetKey(KeyCode.D))
         {
-			offsetX = Time.time * speed;
+			//offsetX = Time.time * speed;
+			offsetX = player.transform.position.x;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            offsetY = Time.time * -speed;
+            //offsetY = Time.time * -speed;
+			offsetY = player.transform.position.y;
         }
         if (Input.GetKey(KeyCode.W))
         {
-			offsetY = Time.time * speed;
+			//offsetY = Time.time * speed;
+			offsetY = player.transform.position.y;
         }
 
 		Vector2 offset = new Vector2(offsetX,offsetY);
